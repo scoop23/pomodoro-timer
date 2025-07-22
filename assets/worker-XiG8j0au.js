024271111e370eec10442311e1e293c295e7eee2
Sebasch Amauri Santillan <sebaschamauri.09@gmail.com>
2025-07-22 16:10:36 +0800

    Updates

--- assets/worker-XiG8j0au.js
+++
@@ -1,1 +0,0 @@
-(function(){"use strict";self.onmessage=function(s){let{strmin:n,strsec:r}=s.data,t=Number(n),e=Number(r),i=setInterval(()=>{t===0&&e===0||(e===0?(t--,e=59):e--,self.postMessage({min:String(t),sec:String(e).padStart(2,"0"),intervalId:i}))},100)}})();
